8fae86a0019cf4948b374b560de21460d930e53a
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-06-01 22:26:10 +0300

    lab7

---
+++ __test__/server.test.js
@@ -0,0 +1,66 @@
+'use strict';
+// const serverMod = require('../lib/server');
+// const server = serverMod.server;
+const {server} = require('../lib/server');
+const supertest = require('supertest');
+const mockRequest = supertest(server);
+
+describe('web server', () => {
+    it('should respond with 500', ()=> {
+        
+        return mockRequest.get('/bad')
+            .then(results=> {
+                expect(results.status).toBe(500);
+            }).catch(console.error);
+    });
+
+    it('should respond 404 of an invalid route',() => {
+
+        return mockRequest
+            .get('/somerandometext')
+            .then(results => {
+                expect(results.status).toBe(404);
+            }).catch(console.log);
+    });
+
+    it('should respond properly /categories', ()=> {
+        return mockRequest
+            .get('/categories?query=shoes')
+            .then(results => {
+                expect(results.status).toBe(200);
+            })
+    });
+    it('should respond properly /products', ()=> {
+        return mockRequest
+            .get('/products?query=nikeshoes')
+            .then(results => {
+                expect(results.status).toBe(200);
+            })
+    });
+
+    it('should post data to products', ()=> {
+        return mockRequest
+            .post('/products')
+            .send({name: "super-sayin tshirt"})
+            .then(results => {
+                expect(results.status).toBe(200);
+            })
+    });
+    it('should post data to products', ()=> {
+        return mockRequest
+            .post('/products')
+            .send({name: "super-sayin tshirt"})
+            .then(results => {
+                expect(results.status).toBe(200);
+            })
+    });
+    it('should post data to catagories', ()=> {
+        return mockRequest
+            .post('/categories')
+            .send({name: "super-sayin tshirt"})
+            .then(results => {
+                expect(results.status).toBe(200);
+            })
+    });
+
+});
