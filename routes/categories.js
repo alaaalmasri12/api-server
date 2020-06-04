53d73730f33f4d5302276f1c469c0bbdadf72a86
alaalmasri12 <alaa_almasri1997@yahoo.com>
2020-06-04 06:47:37 +0300

    class-08 fix eslint

--- routes/categories.js
+++ routes/categories.js
@@ -10,41 +10,41 @@ router.post('/categories', postcategorie);
 router.put('/categories/:id',updatecategorie);
 router.delete('/categories/:id', deletecategorie);
 function getcategorie(req, res, next) {
-    catagory.read()
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(next);
+  catagory.read()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(next);
 }
 function getcategoriebyid(req, res, next) {
-    catagory.read(req.params.id)
-        .then(data => {
-            res.status(200).json(data);
-        })
-        .catch(next);
+  catagory.read(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(next);
 }
 
 function postcategorie(req,res, next ) {
-    let {name ,displayname,description}=req.body;
-    let record={name,displayname,description};
-    catagory.create(record)
-        .then(data => {
-            res.status(201).json(data); 
-        }).catch(next);
+  let {name ,displayname,description}=req.body;
+  let record={name,displayname,description};
+  catagory.create(record)
+    .then(data => {
+      res.status(201).json(data); 
+    }).catch(next);
 }
 function updatecategorie(req,res, next ) {
-    let {name ,displayname,description}=req.body;
-    let record={name,displayname,description};
-    catagory.update(req.params.id,record)
-        .then(data => {
-            res.status(200).json(data); 
-        }).catch(next);
+  let {name ,displayname,description}=req.body;
+  let record={name,displayname,description};
+  catagory.update(req.params.id,record)
+    .then(data => {
+      res.status(200).json(data); 
+    }).catch(next);
 }
 
 function deletecategorie(req,res, next) {
-    catagory.delete(req.params.id)
-        .then(data => {
-            res.status(200).json(data);
-        }).catch(next);
+  catagory.delete(req.params.id)
+    .then(data => {
+      res.status(200).json(data);
+    }).catch(next);
 }
 module.exports=router;
